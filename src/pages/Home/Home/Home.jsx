f68cf4d6af440e8df92f0576d0964f267db5932a
Rony <ronytalukder0014@gmail.com>
2023-10-15 22:27:44 +0600

    first cmm

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,29 @@
+import Banner from "../Banner/Banner";
+import Category from "../Category/Category";
+import PopularMenu from "../PopularMenu/PopularMenu";
+
+const Home = () => {
+    return (
+        <div>
+            <Banner/>
+            <Category/>  
+            <PopularMenu/>
+        </div>
+    );
+};
+
+export default Home;
+
+// video 74-7
+// video 74-7
+// video 74-7
+// video 74-7
+// video 74-7
+// video 74-7
+// video 74-7
+// video 74-7
+// video 74-7
+// video 74-7
+// video 74-7
+// video 74-7
+// video 74-7
