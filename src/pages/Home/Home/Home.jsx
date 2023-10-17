bc818d4980e8cd675de03de52658d49aaf026cd9
Rony <ronytalukder0014@gmail.com>
2023-10-17 23:32:44 +0600

    first cmm

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,23 @@
+import Banner from "../Banner/Banner";
+import Category from "../Category/Category";
+import PopularMenu from "../PopularMenu/PopularMenu";
+
+const Home = () => {
+    return (
+        <div>
+            <Banner/>
+            <Category/>  
+            <PopularMenu/>
+        </div>
+    );
+};
+
+export default Home;
+
+// video 74-7
+// video 74-7
+// video 74-7
+// video 74-7
+// video 74-7 
+
+{/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum aut fuga officia reiciendis, eaque necessitatibus dolor porro nisi expedita et!</p> */}
